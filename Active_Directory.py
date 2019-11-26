21696c58576ea284ec9c065561d0d3870ff4a41b
Abhisek Jana <adeveloperdiary@gmail.com>
2019-11-26 15:13:28 -0500

    Create Active_Directory.py

---
+++ Active_Directory.py
@@ -0,0 +1,61 @@
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child.add_group(sub_child)
+parent.add_group(child)
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+
+    if group is None or user is None:
+        return False
+
+    if group.get_groups() is not None:
+        for g in group.get_groups():
+            return is_user_in_group(user, g)
+
+    if group.get_users() is not None:
+        users = group.get_users()
+        if user in users:
+            return True
+
+    return False
+
+
+print(is_user_in_group("sub_child_user", parent))  # Return True
+print(is_user_in_group("sub_child_user", None))  # Return False
+print(is_user_in_group(None, sub_child))  # Return False
+print(is_user_in_group("unknown_user", parent))  # Return False
