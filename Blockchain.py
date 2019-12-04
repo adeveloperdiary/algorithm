160815011149eb36e3e238a511a1cb24ab628dad
Abhisek Jana <adeveloperdiary@gmail.com>
2019-12-04 00:29:35 -0500

    Update Blockchain.py
    Added delete()

--- Blockchain.py
+++ Blockchain.py
@@ -6,11 +6,8 @@ class Block:
 
     def calc_hash(self):
         sha = hashlib.sha256()
-
         hash_str = (str(datetime.datetime.now()) + "We are going to encode this string of data!").encode('utf-8')
-
         sha.update(hash_str)
-
         return sha.hexdigest()
 
     def __init__(self, timestamp, data, previous_hash):
@@ -23,11 +20,10 @@ class Block:
 
 class BlockChain:
     def __init__(self):
-        self.tail = None
-        self.total_size = 0
         self.head = None
 
     def add(self, data):
+        print("Execute Add")
         now_GMT = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
 
         if self.head is not None:
@@ -35,15 +31,12 @@ class BlockChain:
             node = Block(now_GMT, data, self.head.hash)
             node.prev = temp
             self.head = node
-
         else:
             self.head = Block(now_GMT, data, '0')
-            self.tail = self.head
 
         return self.head.hash
 
     def get(self, hash):
-
         if self.head is not None:
             if self.head.hash == hash:
                 return self.head
@@ -60,18 +53,67 @@ class BlockChain:
             return None
 
     def delete(self, hash):
-        pass
+        print("Execute Delete")
+        if self.head is not None:
+            if self.head.hash == hash:
+                if self.head.prev is not None:
+                    self.head = self.head.prev
+                else:
+                    self.head = None
+            else:
+                node = self.head
+                while node.prev is not None:
+                    next = node
+                    node = node.prev
+                    if node.hash == hash:
+                        if node.prev is None:
+                            next.previous_hash = '0'
+                            next.prev = None
+                            node = None
+                            break
+                        else:
+                            next.previous_hash = node.prev.hash
+                            next.prev = node.prev
+                            node = None
+                            break
+
+    def display(self):
+        str = ''
+
+        if self.head is not None:
+            node = self.head
+            if node.prev is None:
+                str += '( ' + node.data + ' )'
+            else:
+                str += '( ' + node.data + ' )'
+                while node.prev is not None:
+                    node = node.prev
+                    str += '->( ' + node.data + ' )'
+
+        print('Current BlockChain: ' + str)
 
 
 if __name__ == "__main__":
     bc = BlockChain()
     hash1 = bc.add("1")
-    print(hash1)
     hash2 = bc.add("2")
-    print(hash2)
     hash3 = bc.add("3")
-    print(hash3)
+    hash4 = bc.add("4")
+    bc.display()  # print ( 4 )->( 3 )->( 2 )->( 1 )
 
     find = bc.get(hash1)
 
-    print(find.data)
+    print(find.data)  # Print 1
+
+    bc.delete(hash3)
+    bc.display()  # print ( 4 )->( 2 )->( 1 )
+    bc.delete(hash4)
+    bc.display()  # print ( 2 )->( 1 )
+    bc.delete(hash1)
+    bc.display()  # print ( 2 )
+    bc.delete(hash2)
+    bc.display()  # print ''
+    bc.delete(hash1)
+    bc.display()  # print ''
+    find = bc.get(hash1)
+    print(find)  # print None
