add3c98de6b6aee3b3b961ed81b090ce832d9dbf
Abhisek Jana <adeveloperdiary@gmail.com>
2019-12-09 15:30:01 -0500

    Update File_Recursion.py
    added comments

--- File_Recursion.py
+++ File_Recursion.py
@@ -1,3 +1,11 @@
+"""
+Runtime : O(n)
+
+1. Used recursive approach to go through all the files and directories 
+
+"""
+
+
 import os
 
 
@@ -17,21 +25,31 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-
+    
+    # Initialize a list
     arr = list()
+    
+    # if path is empty return empty array
     if path.strip(' ') == "":
         return []
     else:
+        
+        # List the dir's under the path
         paths = os.listdir(path)
+        
+        # set the path as parent
         parent = path
-
+        
+        # If there is no sub items
         if paths is None or len(paths) == 0:
             return []
         else:
+            # Loop through the paths
             for p in paths:
+                # if the item is a file, append in the arr list
                 if os.path.isfile(os.path.join(parent, p)) and p.endswith(suffix):
                     arr.append(os.path.join(parent, p))
-
+                # if the item is a dir, then call find_files() again
                 if os.path.isdir(os.path.join(parent, p)):
                     child_arr = find_files(suffix, os.path.join(parent, p))
                     arr.extend(child_arr)
